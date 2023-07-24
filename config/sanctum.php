6844eb9703c92abcd57d60d26ddcefe4caf2797b
Roman <Romanmakukha1989@gmail.com>
2023-07-24 20:22:02 +0300

    tune_stateful_domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,12 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'movieone.ru, www.movieone.ru, localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS')),
 
     /*
     |--------------------------------------------------------------------------
